6c5dc5159e3d022df8f52076a07c05993dac6643
Babiker Babiker <bbabiker09@gmail.com>
2024-06-11 15:01:26 -0700

    Removed all files from tracking except FinanceFirst.cpp

--- FinanceFirst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/Documents/GitHub/FinanceFirst/build || { echo "Failed to navigate to build directory"; exit 1; }
-
-cmake .. || { echo "CMake configuration failed"; exit 1; }
-
-make || { echo "Build failed"; exit 1; }
-
-./FinanceFirst || { echo "Failed to run FinanceFirst"; exit 1; }
--- build/CMakeFiles/FinanceFirst.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for FinanceFirst.
-# This may be replaced when dependencies are built.
